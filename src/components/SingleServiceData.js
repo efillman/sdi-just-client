e3065fb7e084efdbd51347c72fc81effab86cc76
young <badfriend87@hotmail.com>
2021-08-31 10:16:44 -0700

    Service with single component

---
+++ src/components/SingleServiceData.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SingleServiceData = (props) => {
+  return (
+    <div>This is the service data for service {props.service.service_id}</div>
+  )
+}
+export default SingleServiceData
--- src/components/TrackerView.js
+++ src/components/TrackerView.js
@@ -1,8 +1,59 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+import SingleServiceData from './SingleServiceData';
+
 
 const TrackerView = () => {
+  let serviceIds = [1, 2, 3, 4, 5]
   return (
-    <div>This is the tracker view</div>
+    <Container>
+      <Row className="justify-content-center">
+        {services.data.map((service) => {
+          return <Col xs={12} lg={2}><SingleServiceData service={service} /></Col>
+        })}
+      </Row>
+    </Container>
   )
 }
+
+const services = {
+  "total": 6,
+  "limit": 10,
+  "skip": 0,
+  "data": [
+      {
+          "service_id": 1,
+          "service_short_name": "USA",
+          "service_long_name": "United States Army"
+      },
+      {
+          "service_id": 2,
+          "service_short_name": "USMC",
+          "service_long_name": "United States Marine Corps"
+      },
+      {
+          "service_id": 3,
+          "service_short_name": "USN",
+          "service_long_name": "United States Navy"
+      },
+      {
+          "service_id": 4,
+          "service_short_name": "USAF",
+          "service_long_name": "United States Air Force"
+      },
+      {
+          "service_id": 5,
+          "service_short_name": "USSF",
+          "service_long_name": "United States Space Force"
+      },
+      {
+          "service_id": 6,
+          "service_short_name": "DOD/Other",
+          "service_long_name": "DOD Agencies/Activites"
+      }
+  ]
+}
+
 export default TrackerView
